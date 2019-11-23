870f98e0debaec6c0d7dd9f654c005f1bf4144de
Wendy Harris <wendyv510@hotmail.com>
2019-11-23 16:50:24 +0000

    first commit

---
+++ lib/eric_carle_books.rb
@@ -0,0 +1,6 @@
+require "eric_carle_books/version"
+
+module EricCarleBooks
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/eric_carle_books/version.rb
@@ -0,0 +1,3 @@
+module EricCarleBooks
+  VERSION = "0.1.0"
+end
