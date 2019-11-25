3487e09530c491c41d6a5074e81356bd27b558dd
Wendy Harris <wendyv510@hotmail.com>
2019-11-25 22:53:47 +0000

    environment changes

--- congif/environment.rb
+++
@@ -1,9 +0,0 @@
-require 'bundler'
-
-Bundle.require 
-
-require_all './lib/eric_carle_books'
-
-require 'nokogiri'
-require 'pry' 
-
---
+++ lib/eric_carle_books/eric_carle_books.rb
@@ -0,0 +1,10 @@
+require "eric_carle_books/version"
+require 'pry'
+require_relative './scraper.rb'
+require 'nokogiri' 
+
+
+module EricCarleBooks
+  class Error < StandardError; end
+  # Your code goes here...
+end
