6e558f9a92539ad6ece36e4acea8be2b5e2fb3bc
Wendy Harris <wendyv510@hotmail.com>
2019-11-26 19:08:19 +0000

    instance variables

--- lib/eric_carle_books/scraper.rb
+++ lib/eric_carle_books/scraper.rb
@@ -1,18 +1,20 @@
 class EricCarleBooks::Scraper 
   
+  attr_accessor :title, :year_published, :description 
+  
   def get_page 
     doc = Nokogiri::HTML(open("http://www.eric-carle.com/ECbooks.html"))
   end 
   
   def get_books 
-    self.get_page.css("span.booktitle")
+    title = self.get_page.css("span.booktitle")
     book = Books.new 
   end 
 
   def get_info(title) 
     self.get_books.select {|book| book.title}
-     # book.year_published = doc.css("span").text[0] 
-      #book.description = doc.css ("span").text [1] 
+     year_published = doc.css("span").text[0] 
+     description = doc.css ("span").text [1] 
   end 
       
   
