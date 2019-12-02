e1cb0db1448b9542c659ed81a6e45a53c3fe0349
Wendy Harris <wendyv510@hotmail.com>
2019-12-02 19:51:55 +0000

    refactoring

--- lib/eric_carle_books/scraper.rb
+++ lib/eric_carle_books/scraper.rb
@@ -1,25 +1,12 @@
 class EricCarleBooks::Scraper 
-  
-  
-  def book_list 
-    @book_list = book_list 
-    book_list = []
-    book_list << title
-  end 
-  
-  def self.get_books
-    @doc = doc 
-    doc = Nokogiri::HTML(open("http://www.eric-carle.com/ECbooks.html"))
-    title = doc.css("span.booktitle")
-    book_list 
-  end 
 
-  def self.get_info(title) 
-     Books.all.select {|book| book.title}
-     year_published = doc.css("span").text[0] 
-     description = doc.css ("span").text [1] 
+  
+  def get_books
+    @doc = Nokogiri::HTML(open("http://www.eric-carle.com/ECbooks.html"))
+    Book = EricCarleBooks::Books.new 
+    Books.title = doc.css("span.booktitle").text 
+    Books.year_published = doc.css("span").text[0]
+    Books.description = doc.css ("span").text [1] 
   end 
       
-  
-  
 end 
