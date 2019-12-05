74ffb8f0d41f8bbd16d3e09c39e1f4c45f9e04ea
Wendy Harris <wendyv510@hotmail.com>
2019-12-05 00:36:23 +0000

    each with index

--- lib/eric_carle_books/scraper.rb
+++ lib/eric_carle_books/scraper.rb
@@ -18,7 +18,7 @@ class EricCarleBooks::Scraper
     @doc = Nokogiri::HTML(open("https://www.teachervision.com/authors/top-10-books-eric-carle"))
     
     titles = @doc.css("div.collection-title")
-    titles.each with_index |title,index|
+    titles.each_with_index do |title, index|
       title=title.text
       description = @doc.css("div.collection-body")[index].text 
        book = EricCarleBooks::Books.new(title,description)
