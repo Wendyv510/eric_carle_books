7e4cc73ccb3b10dfb0083933c912b233d2334561
Wendy Harris <wendyv510@hotmail.com>
2019-12-05 00:38:16 +0000

    delete comments

--- lib/eric_carle_books/scraper.rb
+++ lib/eric_carle_books/scraper.rb
@@ -1,18 +1,5 @@
 class EricCarleBooks::Scraper 
 
-  
-  #def self.get_books
-   # @doc = Nokogiri::HTML(open("http://www.eric-carle.com/ECbooks.html"))
-    
-    #title = @doc.css("span.booktitle").text 
-    #year_published = @doc.css("span").text[0]
-    #book.description = @doc.css ("span").text [1]
-    #titles = @doc.css("span.booktitle")
-     #  titles.each do |title|
-         
-      # end 
-    #book = EricCarleBooks::Books.new(title,year_published)
-  #end 
       
   def self.get_books 
     @doc = Nokogiri::HTML(open("https://www.teachervision.com/authors/top-10-books-eric-carle"))
