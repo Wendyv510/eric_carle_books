f2b36b1ac1e48a652baba8f6f4a4b6f8b21c4616
Wendy Harris <wendyv510@hotmail.com>
2019-12-06 19:49:27 +0000

    link

--- lib/eric_carle_books/scraper.rb
+++ lib/eric_carle_books/scraper.rb
@@ -3,13 +3,14 @@ class EricCarleBooks::Scraper
       
   def self.get_books 
     @doc = Nokogiri::HTML(open("https://www.teachervision.com/authors/top-10-books-eric-carle"))
-    
+    root_url = "https://www.teachervision.com"
     titles = @doc.css("div.collection-title")
     titles.each_with_index do |title, index|
       title=title.text
       description = @doc.css("div.collection-body")[index].text 
-      activity = @doc.css("div.collection-body a").each {|anchor|anchor["href"]}[index]
-       book = EricCarleBooks::Books.new(title,description,activity)
+      link= @doc.css("div.collection-body a")[index].attributes["href"].value
+      activity = root_url + link 
+      book = EricCarleBooks::Books.new(title,description,activity)
     end 
   end 
 end 
